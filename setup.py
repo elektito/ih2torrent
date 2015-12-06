f61b30f98670695b6d3fc7bd57e9bf8adea347cd
Mostafa Razavi <mostafa@sepent.com>
2015-12-06 10:52:49 +0330

    Add distutils files.

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+from pip.req import parse_requirements
+from pip.download import PipSession
+
+import pypandoc
+
+# convert markdown to reStructured Text
+rst = pypandoc.convert('README.md', 'rst', format='markdown')
+
+# writes converted file
+with open('README.rst','w') as outfile:
+    outfile.write(rst)
+
+# read requirements from requirements.txt
+requirements = parse_requirements('requirements.txt', session=PipSession())
+requirements = [str(r.req) for r in requirements]
+
+setup(
+    name = 'ih2torrent',
+    py_modules = ["ih2torrent"],
+    install_requires = requirements,
+    version = '0.1',
+    description = 'Convert a torrent infohash or magnet URI to a .torrent file using DHT and metadata protocol.',
+    author = 'Mostafa Razavi',
+    author_email = 'mostafa@sepent.com',
+    url = 'https://github.com/elektito/ih2torrent',
+    download_url = 'https://github.com/elektito/ih2torrent/tarball/0.1',
+    keywords = ['bittorrent', 'torrent', 'infohash', 'magnet', 'dht', 'metadata', 'metainfo'],
+    classifiers = [
+        "Programming Language :: Python :: 3"
+    ],
+    entry_points = {
+        "console_scripts": [
+            "ih2torrent=ih2torrent:main",
+        ],
+    },
+)
